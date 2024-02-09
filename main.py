8443d9ffd5560f4ac775a103b62c2e9ff99b5b93
ErickFireworks <erick@fireworks.studio>
2024-02-09 17:18:48 -0600

    Add enviroment variable in script main

--- main.py
+++ main.py
@@ -5,61 +5,67 @@ from drive_upload_files import GoogleDriveUploadFiles
 from website_backup import WebsiteBackup
 from database_backup import DatabaseBackup
 from datetime import date
+from dotenv import dotenv_values
 
 import json
 
 if __name__ == "__main__":
-  today = date.today()
-  file_paths = []
+    config = dotenv_values(".env")
+    site = config["SITE"]
+    database = config["DATABASE"]
+    backup_path = config["BACKUP_FILES"]
+    db_password = config["DB_PASSWORD"]
+    today = date.today()
+    file_paths = []
 
-  # Instance
-  token_drive = TokenOuthGoogleDrive()
-  service = token_drive.getCredentialFromDrive()
+    # Instance
+    token_drive = TokenOuthGoogleDrive()
+    service = token_drive.getCredentialFromDrive()
 
-  website_backup = WebsiteBackup(today)
-  database_backup = DatabaseBackup(today)
+    website_backup = WebsiteBackup(today, site, backup_path)
+    database_backup = DatabaseBackup(today, site, database, db_password)
 
-  handle_folder = ActionsFolderFromGoogleDrive(service, today)
-  handle_upload = GoogleDriveUploadFiles(service)
+    handle_folder = ActionsFolderFromGoogleDrive(service, today)
+    handle_upload = GoogleDriveUploadFiles(service)
 
-  folder_main_id = handle_folder.getIdFolderMain()
+    folder_main_id = handle_folder.getIdFolderMain()
 
-  print("\nWelcome to the backup system.\n")
+    print("\nWelcome to the backup system.\n")
 
-  choice = ''
+    choice = ""
 
-  while choice != 'q':
+    while choice != "q":
 
-    print("\n[1] Backup WebSite.")
-    print("[2] Backup Database.")
-    print("[3] Upload Backup to Google drive.")
-    print("[q] Enter q to quit.\n")
+        print("\n[1] Backup WebSite.")
+        print("[2] Backup Database.")
+        print("[3] Upload Backup to Google drive.")
+        print("[q] Enter q to quit.\n")
 
-    choice = input("\nWhat would you like to do?\n")
+        choice = input("\nWhat would you like to do?\n")
 
-    if choice == "1":
-      path_file_zip = website_backup.createWebsiteBackup()
-      file_paths.append(path_file_zip)
-      with open(f"./backup-{today}/list_files.json", "w") as token:
-        token.write(json.dumps(file_paths))
+        if choice == "1":
+            path_file_zip = website_backup.createWebsiteBackup()
+            file_paths.append(path_file_zip)
+            with open(f"./backup-{today}/list_files.json", "w") as token:
+                token.write(json.dumps(file_paths))
 
-    elif choice == "2":
-      path_file_sql = database_backup.createDatabaseBackup()
-      file_paths.append(path_file_sql)
-      with open(f"./backup-{today}/list_files.json", "w") as token:
-        token.write(json.dumps(file_paths)) 
+        elif choice == "2":
+            path_file_sql = database_backup.createDatabaseBackup()
+            file_paths.append(path_file_sql)
+            with open(f"./backup-{today}/list_files.json", "w") as token:
+                token.write(json.dumps(file_paths))
 
-    elif choice == "3":
-      # list_files = list(open(f"./backup-{today}/list_files.json", "r"))
-      with open(f"./backup-{today}/list_files.json") as f:
-         list_files = json.loads(f.read())
-      handle_upload.uploadFile( list_files, folder_main_id, handle_folder)
-      handle_folder.deleteFolderTempFiles()
+        elif choice == "3":
+            # list_files = list(open(f"./backup-{today}/list_files.json", "r"))
+            with open(f"./backup-{today}/list_files.json") as f:
+                list_files = json.loads(f.read())
+            handle_upload.uploadFile(list_files, folder_main_id, handle_folder)
+            handle_folder.deleteFolderTempFiles()
 
-    elif choice == "q":
-      print("\nThank you for using the backup system.\n")
-      file_paths = []
-      break
-    
-    else:
-      print("\nI don't understand the choice, please try again.!!!.\n")
+        elif choice == "q":
+            print("\nThank you for using the backup system.\n")
+            file_paths = []
+            break
+
+        else:
+            print("\nI don't understand the choice, please try again.!!!.\n")
